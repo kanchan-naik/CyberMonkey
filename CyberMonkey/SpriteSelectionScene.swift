af4f96d79cf224fec21822377c29b7a48e6091b0
Catherine Lu <catherinelu@uchicago.edu>
2023-11-10 14:58:45 -0600

    avatar selection page files

---
+++ CyberMonkey/SpriteSelectionScene.swift
@@ -0,0 +1,13 @@
+//
+//  SpriteSelectionScene.swift
+//  CyberMonkey
+//
+//  Created by Catherine Lu on 11/3/23.
+//  Copyright © 2023 Jonathan Yang. All rights reserved.
+//
+
+import SpriteKit
+
+class SpriteSelectionScene: SKScene {
+    
+}
