87e8eef29ba1a617b05e33008f64f9cf60bd439c
kanchan-naik <102567298+kanchan-naik@users.noreply.github.com>
2023-11-13 09:01:45 -0600

    Update QuestionBank.swift

--- CyberMonkey/QuestionBank.swift
+++ CyberMonkey/QuestionBank.swift
@@ -12,6 +12,22 @@ class QuestionBank {
     var list = [Question]()
     
     init() {
+
+    // User Privacy
+        list.append(Question(questionText: "It can help your phone battery last longer.", choiceA: "Anything you want to buy", choiceB: "It makes it easier for your parents to find you.", choiceC: "It makes it easier for your parents to find you.", choiceD: "It makes it easier for your friends to find you.", correctAnswerText: 1))
+        list.append(Question(questionText: "If a stranger asks you to share your location, what should you do?", choiceA: "Say no and tell them you can't.", choiceB: "Ask them why they need to know your location.", choiceC: "Turn on your location settings and tell them where you are..", choiceD: "Ask your parents if it's okay to turn on your location settings.", correctAnswerText: 0))
+        list.append(Question(questionText: "What should you do before you post something on social media?", choiceA: "Think about how it would make you feel if someone said or did the same thing to you.", choiceB: "Ask yourself if you would be okay with your parents seeing it.", choiceC: "Just post it without thinking about it.", choiceD: "Ask your friends if it's okay to post it.", correctAnswerText: 0))
+        list.append(Question(questionText: "Who should you be friends with on social media?", choiceA: "Everyone you meet online", choiceB: "People you know and trust in real life", choiceC: "People who ask to be your friend", choiceD: "Anyone who follows you", correctAnswerText: 1))
+        list.append(Question(questionText: "What should you do if someone you don't know sends you a message on social media?", choiceA: "Respond to their message and tell them about yourself.", choiceB: "Block them and tell a trusted adult.", choiceC: "Add them as a friend and see what they have to say.", choiceD: "Ignore them and hope they go away.", correctAnswerText: 1))
+
+    // Digital Footprint
+        list.append(Question(questionText: "What is the best way to protect your privacy on social media?", choiceA: "Share your personal information with everyone you meet online.", choiceB: "Don't worry about privacy, no one cares about your information.", choiceC: "Only share information with people you know and trust.", choiceD: "Make all of your posts public so everyone can see them.", correctAnswerText: 2))
+        list.append(Question(questionText: "What is a digital footprint?", choiceA: "The trail of information that you leave behind as you use the internet and electronic devices.", choiceB: "The amount of time you spend on the internet each day.", choiceC: "The number of friends you have on social media.", choiceD: "Your passwords for Facebook and Instagram.", correctAnswerText: 0))
+        list.append(Question(questionText: "What are some examples of things that can add to your digital footprint?", choiceA: "The websites you visit, the emails you send, and the posts you make on social media.", choiceB: "The computer model you have.", choiceC: "Your passwords for Facebook and Instagram.", choiceD: "A downloaded song on your computer.", correctAnswerText: 0))
+        list.append(Question(questionText: "Why is it important to be aware of your digital footprint?", choiceA: "Because it can affect your reputation and privacy.", choiceB: "Because it can be used to hack into your social media accounts.", choiceC: "Because it is sent to your parents as a letter every month.", choiceD: "Because it is checked by your teachers and graded.", correctAnswerText: 0))
+
+
+    // Original Questions
         list.append(Question(questionText: "What is your digital footprint?", choiceA: "Anything you want to buy", choiceB: "A picture of your foot", choiceC: "Everything on the internet about you", choiceD: "Your parent’s details", correctAnswerText: 2))
         list.append(Question(questionText: "I can stop strangers contacting me by...", choiceA: "Setting privacy to Private/ Friends", choiceB: "Why worry? Strangers won’t get in touch", choiceC: "Ignore them", choiceD: "Setting privacy to public", correctAnswerText: 0))
         list.append(Question(questionText: "If a stranger contacts me, I will...", choiceA: "Tell a trusted adult", choiceB: " Block them", choiceC: "Ignore them", choiceD: " All the above", correctAnswerText: 3))
@@ -30,7 +46,6 @@ class QuestionBank {
         list.append(Question(questionText: "Do your friends have privacy switched on if you do?", choiceA: "No", choiceB: "Yes", choiceC: "Probably", choiceD: "I will have to ask him", correctAnswerText: 3))
         list.append(Question(questionText: "Online reputation is...", choiceA: "If you have been hacked", choiceB: "How you are feeling", choiceC: "Your nickname", choiceD: "What you and others say about yourself", correctAnswerText: 3))
         list.append(Question(questionText: "Who can have their identity stolen?", choiceA: "Just adults", choiceB: "Just teachers", choiceC: "Just children", choiceD: "Everyone", correctAnswerText: 3))
-        
-        
+
     }
 }
